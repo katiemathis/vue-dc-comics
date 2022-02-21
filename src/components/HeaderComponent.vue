a52d5d085f2f55969b502a3f1cffd1e38e33f57d
katiepalumbo <palumbo.katie@gmail.com>
2022-02-21 16:53:55 +0100

    header layout

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="header-container">
-    <div>
+    <div class="logo">
         <img alt="Vue logo" src="../assets/dc-logo.png">
     </div>
-    <div>
+    <div class="menu">
         <ul>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+            <li><a href="#" target="_blank" rel="noopener">CHARACTERS</a></li>
+            <li><a href="#" target="_blank" rel="noopener">COMICS</a></li>
+            <li><a href="#" target="_blank" rel="noopener">MOVIES</a></li>
+            <li><a href="#" target="_blank" rel="noopener">TV</a></li>
+            <li><a href="#" target="_blank" rel="noopener">GAMES</a></li>
+            <li><a href="#" target="_blank" rel="noopener">COLLECTIBLES</a></li>
+            <li><a href="#" target="_blank" rel="noopener">VIDEOS</a></li>
+            <li><a href="#" target="_blank" rel="noopener">FANS</a></li>
+            <li><a href="#" target="_blank" rel="noopener">NEWS</a></li>
+            <li><a href="#" target="_blank" rel="noopener">SHOP</a></li>
         </ul>
     </div>
   </div>
@@ -21,6 +29,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.header-container {
+    display: flex;
+    justify-content: space-around;
+}
+
+.logo {
+    width: 40%;
+    
+}
+
+img {
+    max-height: 60px;
+}
+
+.menu {
+    width: 60%;
+    padding-right: 10%;
+}
+
 h3 {
   margin: 40px 0 0;
 }
@@ -31,8 +59,12 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
+  font-size: 11px;
+  font-weight: 600;
+  vertical-align: center;
 }
 a {
-  color: #42b983;
+  color: #000;
+  text-decoration: none;
 }
 </style>
